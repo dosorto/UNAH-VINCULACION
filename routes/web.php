08164ea4b1f9bad312e25d33d0dedf841d79cd36
Ernesto Noe Moncada Valladares <ernesto.moncada@unah.hn>
2024-09-28 17:37:46 -0600

    Login completo

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,25 @@ use App\Livewire\Demografia\CreatePais;
 use App\Livewire\Demografia\Pais\ListPaises;
 use App\Livewire\Demografia\Departamento\CreateDepartamento;
 use App\Livewire\Demografia\Departamento\ListDepartamentos;
+use App\Livewire\Login\Login;
 
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', Login::class)
+->name('login');
+
+
+Route::middleware(['auth'])->group(function () {
+    
+    Route::get('crearPais', CreatePais::class)
+        ->name('crear-pais');
+    
+    Route::get('listarPais', ListPaises::class)
+        ->name('lista-paises');
+    
+    Route::get('crearDepartamento', CreateDepartamento::class)
+        ->name('crearDepartamento');
+    
+        Route::get('ListarDepartamentos', ListDepartamentos::class)
+        ->name('ListarDepartamentos');
+        
 });
-
-
-Route::get('crearPais', CreatePais::class)
-    ->name('crear-pais');
-
-Route::get('listarPais', ListPaises::class)
-    ->name('lista-paises');
-
-
-Route::get('crearDepartamento', CreateDepartamento::class)
-    ->name('crearDepartamento');
-
-    Route::get('ListarDepartamentos', ListDepartamentos::class)
-    ->name('ListarDepartamentos');
