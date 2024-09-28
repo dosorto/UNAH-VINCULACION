b2d620cc90ebfe638569da4cdab4a8ccea541844
ernestomoncada14 <ernesto.moncada@unah.hn>
2024-09-28 16:04:01 -0600

    Roles casi listo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,9 +16,25 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+         // Crear un rol
+         $role = Role::create(['name' => 'admin']);
+
+         // Crear un permiso
+         $permission = Permission::create(['name' => 'edit articles']);
+ 
+         // Asignar un rol a un usuario (con contraseña encriptada)
+         $user = User::create([
+             'name' => 'neto',
+             'email' => 'neto@unah.hn',
+             'password' => bcrypt('123'), // Asegurarse de encriptar la contraseña
+         ]);
+         $user->assignRole('admin')->save();
+ 
+         // Asignar permisos a roles
+         $role->givePermissionTo('edit articles')->save();
     }
 }
