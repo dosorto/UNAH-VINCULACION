58cd480922772bcdbc7a90afa017613eca7de452
Francisco Paz <fjpazf@unah.hn>
2024-10-16 00:12:42 -0600

    correcion en modelos

--- app/Livewire/Proyectos/Vinculacion/CreateProyectoVinculacion.php
+++ app/Livewire/Proyectos/Vinculacion/CreateProyectoVinculacion.php
@@ -11,11 +11,9 @@ use Livewire\Component;
 use Illuminate\Contracts\View\View;
 use Filament\Forms\Components\Wizard;
 
-
-use App\Models\Unidad_Academica\FaFacultad_centro;
-use App\Models\Unidad_Academica\Entidad_academica;
-use App\Models\Unidad_Academica\Carrera;
-
+use App\Models\UnidadAcademica\FacultadCentro;
+use App\Models\UnidadAcademica\EntidadAcademica;
+use App\Models\UnidadAcademica\Carrera;
 
 
 use Filament\Forms\Components\Select;
@@ -45,20 +43,27 @@ class CreateProyectoVinculacion extends Component implements HasForms
                                 ->required()
                                 ->maxLength(255),
                             Select::make('facultades_centros')
-                                ->options(FaFacultad_centro::All()
+                                ->options(FacultadCentro::All()
                                     ->pluck('nombre', 'id'))
+                                    ->label('Facultades o Centros')
                                 ->searchable()
-                                    ->required()
-                                
-                                ,
-                            Forms\Components\TextInput::make('carrera_facultad_centro_id')
-                                ->numeric(),
+                                ->required(),
+                            Select::make('entidades_academicas')
+                                ->options(EntidadAcademica::All()
+                                    ->pluck('entidad_academica', 'id'))
+                                ->searchable()
+                                ->required(),
+                            Select::make('carreras')
+                                ->options(Carrera::All()
+                                    ->pluck('carrera', 'id'))
+                                ->searchable(),
                             Forms\Components\TextInput::make('entidad_academica_id')
                                 ->numeric(),
                             Forms\Components\TextInput::make('ods_id')
                                 ->required()
                                 ->numeric(),
-                        ]),
+                        ])
+                        ->columns(2),
                     Wizard\Step::make('II.')
                         ->description('Integrantes del equipo universitario')
                         ->schema([
