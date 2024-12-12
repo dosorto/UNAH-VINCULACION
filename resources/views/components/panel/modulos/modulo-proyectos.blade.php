9d0340783c96954257fbea15460e6ab6c04d6227
Kenis Noe Osorto Reyes <kenisreyes11@gmail.com>
2024-12-12 11:58:04 -0600

    cambios kenis

--- resources/views/components/panel/modulos/modulo-proyectos.blade.php
+++ resources/views/components/panel/modulos/modulo-proyectos.blade.php
@@ -7,7 +7,7 @@
         <x-panel.navbar-horizontal.item titulo="Proyectos" ruta="listarProyectosVinculacion" permiso="proyectos-admin-proyectos" />
         <x-panel.navbar-horizontal.item titulo="Solicitud Proyecto" ruta="listarProyectosSolicitado" permiso="proyectos-admin-solicitados" />
         <x-panel.navbar-horizontal.item titulo="Informe Intermedio" ruta="listarInformesSolicitado" permiso="proyectos-admin-solicitados" />
-        <x-panel.navbar-horizontal.item titulo="Revision Final" ruta="listarProyectoRevisionFinal" permiso="proyectos-admin-solicitados" />
+        <x-panel.navbar-horizontal.item titulo="Revisión Final" ruta="listarProyectoRevisionFinal" permiso="proyectos-admin-solicitados" />
     </x-panel.navbar-horizontal.navbar-horizontal>
 @endsection
 
