506e7b35b4a34e412105c4a41dcc7f13a10e4a6f
Vanessa Nicolle Lainez Zambrano <znicolle63@gmail.com>
2025-05-28 15:19:31 -0600

    otro cambio :(

--- resources/views/aplicacion/login2.blade.php
+++ resources/views/aplicacion/login2.blade.php
@@ -26,7 +26,7 @@ contentPosition="beside"
             
             <div class="relative">
                 <div class="absolute inset-0 flex items-center">
-                    <div class="w-full border-t border-gray-300 dark:border-gray-700"></div>
+                    <div class="w-full border-t border-gray-600 dark:border-gray-700"></div>
                 </div>
                 <div class="relative flex justify-center text-sm">
                     <span class="px-2 bg-white dark:bg-gray-800  text-gray-500 dark:text-gray-400">Iniciar sesión con</span>
