3e8dea50c4e8f3d52281c40a218c385723de8525
acxelaplicano01 <acxel.aplicano@unah.hn>
2025-07-02 14:57:16 -0600

    .

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/apexcharts.min.js', 'resources/js/flowbite.min.js'],
             refresh: [
                 ...refreshPaths,
                 'app/Filament/**',
