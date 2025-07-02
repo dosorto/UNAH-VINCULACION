3364104d7180ac0b7208f541d6f062a357e77a62
acxelaplicano01 <acxel.aplicano@unah.hn>
2025-07-02 14:59:30 -0600

    .

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
-
+import './apexcharts.min.js';
+import './flowbite.min.js';
 
 document.addEventListener('livewire:navigated', () => {
     const themeToggleBtn = document.getElementById('theme-toggle');
