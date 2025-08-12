c9e2ac4f4be053b86dae9a979e6b1d6e86033d69
acxelaplicano01 <acxel.aplicano@unah.hn>
2025-08-12 15:16:26 -0600

    panel de proyectos correjido

--- resources/views/livewire/inicio/dashboards/dasboard-docente.blade.php
+++ resources/views/livewire/inicio/dashboards/dasboard-docente.blade.php
@@ -5,11 +5,11 @@
                     <div class="container mx-auto">
                         <div class="mb-6">
                             <div class="w-full mb-6 lg:mb-0">
-                                <div class="p-6 bg-yellow-500 dark:bg-yellow-500 rounded-xl">
+                                <div class="p-6 bg-yellow-500 dark:bg-yellow-700 rounded-xl">
                                     <div class="container px-4 mx-auto">
                                         <div class="relative overflow-hidden">
                                             <div class="relative max-w-sm mx-auto lg:mx-0 mb-2 lg:mb-0">
-                                                <h3 class="text-3xl font-bold text-white">Bienvenido a su Dashboard
+                                                <h3 class="text-2xl font-bold text-white">Bienvenido a su Panel estadístico
                                                 </h3>
                                                 <p class="font-medium text-blue-200">Aquí podrás darle seguimiento a
                                                     tus
