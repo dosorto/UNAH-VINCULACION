989752b1aace1c60a485ac6ca4347c8a505c51e7
Vanessa Nicolle Lainez Zambrano <znicolle63@gmail.com>
2025-08-21 11:41:11 -0600

    .

--- app/Services/Constancia/BuilderConstancia.php
+++ app/Services/Constancia/BuilderConstancia.php
@@ -125,7 +125,7 @@ private function generarCorrelativo(): string
                 'destinatario_type' => Empleado::class,
                 'destinatario_id' => $this->empleadoProyecto->empleado_id,
                 'tipo_constancia_id' => $tipoId,
-                'status' => true,
+                'status' => 'generada',
                 'correlativo' => $this->generarCorrelativo(),
                 'hash' => Str::random(32)
             ]);
